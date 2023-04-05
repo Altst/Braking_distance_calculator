028b2e382e05b1eb4b4c159b77d904e9e9e339c2
Witcher-MTM <phantomit228@gmail.com>
2023-04-05 12:52:59 +0300

    add valid
    Check if number < 0 throw alert
    
    Co-Authored-By: margoshkari <68592214+margoshkari@users.noreply.github.com>

--- src/app/stop-way-calc/stop-way-calc.component.ts
+++ src/app/stop-way-calc/stop-way-calc.component.ts
@@ -12,7 +12,9 @@ export class StopWayCalcComponent {
   Calc(){
     if(Number.isInteger(Number(this.killometrs)) && 
       Number.isInteger(Number(this.weight)) &&
-      this.killometrs && this.weight)
+      this.killometrs && this.weight && 
+      Number(this.weight)>0 &&
+      Number(this.killometrs)>0)
     {
       var metrSec:number = parseInt(this.killometrs)*1000/3600;
       this.res =  Math.round(Number((metrSec^2/(2*0.7*9.81)) + (metrSec * (metrSec / (2 * 0.7 * 9.81 * (1500 / parseInt(this.weight)))))));
