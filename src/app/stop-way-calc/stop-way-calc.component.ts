867d40661c779a41ce9642bbfa1da67969cc0fb9
Witcher-MTM <phantomit228@gmail.com>
2023-04-05 12:25:09 +0300

    add scc
    Co-Authored-By: margoshkari <68592214+margoshkari@users.noreply.github.com>

--- src/app/stop-way-calc/stop-way-calc.component.ts
+++ src/app/stop-way-calc/stop-way-calc.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./stop-way-calc.component.scss']
 })
 export class StopWayCalcComponent {
-  km: number = 0;
-  weight: number = 0;
+  killometrs: string = '';
+  weight: string = '';
   res: number = 0;
   Calc(){
-    var metrSec:number = this.km*1000/3600;
-    this.res =  (metrSec^2/(2*0.7*9.81)) + (metrSec * (metrSec / (2 * 0.7 * 9.81 * (1500 / this.weight))))
     
+    var metrSec:number = parseInt(this.killometrs)*1000/3600;
+    this.res =  Math.round(Number((metrSec^2/(2*0.7*9.81)) + (metrSec * (metrSec / (2 * 0.7 * 9.81 * (1500 / parseInt(this.weight)))))));
   }
 }
